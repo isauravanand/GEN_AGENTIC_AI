88c835fcbab76c62b4111e2cfe322d8ee2a83f98
Saurav Anand <sauravanand1608@gmail.com>
2026-08-10 20:14:16 +0530

    Learning of Day3

---
+++ week1/day3/tokens.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)
+
+model="llama-3.3-70b-versatile"
+role="user"
+
+#promts
+
+promt1="hi"
+promt2="Explain Machine learning in detail"
+promt3="Write a 1000 word eassy on Agentic AI"
+
+prompts=[promt1,promt2,promt3]
+for prompt in prompts:
+    message={
+        "role": role,
+        "content": prompt
+    }
+    messages=[message]
+    response=client.chat.completions.create(model=model, messages=messages)
+    usage=response.usage
+    print(
+        f"Prompt: {prompt} --> your tokens: {usage.prompt_tokens} "
+        f"completion_tokens: {usage.completion_tokens} "
+        f"total_tokens: {usage.total_tokens} "
+        f"Finish Reason: {response.choices[0].finish_reason}"
+    )
+
+
+
+# # message me role and content
+
+
+# # print(response)
+
+# print("#######################################\n")
+
+# answer=response.choices[0].message.content
+# print(answer)
